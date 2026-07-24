2e5f465372b623f34cc8ed2d60c9dc67ed9a02d1
Adones Melo <adones.n.m@outlook.com>
2026-07-24 00:07:19 +0000

    Desafio: nível novato

--- Main.java
+++ Main.java
@@ -1,10 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        Personagem heroi1 = new Personagem("Arthus", "Guerreiro", 1, 100, 10.5);
+        Personagem p1 = new Personagem("Thor", "Guerreiro", 5, 80, 12.5);
+        Personagem p2 = new Personagem("Merlin", "Mago", 4, 90, 10.5);
+        Personagem p3 = new Personagem("Legolas", "Arqueiro", 3, 100, 14.5);
 
-        .
-        .
-        .
-
+        p1.exibirStatus();
+        p2.exibirStatus();
+        p3.exibirStatus();
     }
 }
